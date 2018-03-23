baa09a2f37b24f3694c9d4ba247b824c75d7d825
marcelo10 <marcelo79@gmail.com>
2018-03-23 18:24:26 -0400

    Added morgan

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ const processport = process.env.PROCESS_PORT;
 
 app.use(require('morgan')('combined', { stream: logger.stream }));
 
+app.use((err, req, res, next) => {
+  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  res.status(err.status || 500);
+
+  res.render('error');
+});
+
 app.listen(processport, () => {
   logger.info(`Process up at port ${processport}`);
 });
 
-app.all('/*', (req, res) => {
+app.all('/pid', (req, res) => {
   res.end(`process ${process.pid} says hello!`);
 });
 
