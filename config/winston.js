da0f11ca2e49c4b1c3f4068083dae6b9501fdad1
Marcelo <marcelo79@gmail.com>
2018-10-08 08:54:12 -0500

    adding papertrail for prod logging

--- config/winston.js
+++ config/winston.js
@@ -1,6 +1,11 @@
 const appRoot = require('app-root-path');
 const winston = require('winston');
 
+
+/* eslint-disable no-unused-expressions */
+require('winston-papertrail').Papertrail;
+/* eslint-enable no-unused-expressions */
+
 const tsFormat = () => (new Date()).toLocaleTimeString();
 
 const options = {
@@ -16,7 +21,7 @@ const options = {
   console: {
     level: 'debug',
     handleExceptions: true,
-    json: false,
+    json: true,
     colorize: true,
     timestamp: tsFormat,
   },
@@ -25,7 +30,10 @@ const options = {
 // instantiate a new Winston Logger with the settings defined above
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL,
-  format: winston.format.simple(),
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+  ),
   transports: [
     new winston.transports.Console(options.console),
   ],
@@ -34,7 +42,22 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV === 'dev') {
   logger.add(new winston.transports.File(options.file));
 }
+if (process.env.NODE_ENV === 'prd') {
+  if (process.env.PPT_HOST !== '' && process.env.PPT_PORT !== '') {
+    const winstonPapertrail = new winston.transports.Papertrail({
+      host: process.env.PPT_HOST,
+      port: process.env.PPT_PORT,
+    });
 
+
+    winstonPapertrail.on('error', (err) => { // eslint-disable-line no-unused-vars
+      // Handle, report, or silently ignore connection errors and failures
+    });
+    logger.add(winstonPapertrail);
+  } else {
+    logger.crit('Papertrail envirment not set');
+  }
+}
 // create a stream object with a 'write' function that will be used by `morgan`
 logger.stream = {
   write(message) {
